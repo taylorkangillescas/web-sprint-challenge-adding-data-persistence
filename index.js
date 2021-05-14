a91f517b495fa7dcc1b5236f493359ce26aee2ec
Taylor Kang-Illescas <taylorkangillescas@Taylors-MacBook-Pro-8.local>
2021-05-14 16:04:50 -0700

    updates + sqlite issues

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server');
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-    console.log(`And we're up on port ${PORT}!!`);
+    console.log(`Server on port ${PORT}!!`);
 });
